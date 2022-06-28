8d5786d340544b807a010de2094e508b11e75999
Mahdasah <headbanger.87@hotmail.com>
2022-06-28 02:25:20 -0300

    update

--- components/Styles/Botao/styles.js
+++ components/Styles/Botao/styles.js
@@ -1,11 +1,7 @@
 import styled from "styled-components";
 
 export const Botao = styled.button`
-<<<<<<< HEAD
-	padding: 4px;
-=======
-	padding: 25px;
->>>>>>> 93e1e4100e670cfd037fd515457cc13470a5d077
+	padding: 45px;
 	background-color: blue;
 	color: black;
 	border-radius: 15px;
