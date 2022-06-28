01f17357749c5d96d32a04846c4b42518c46ec64
soutovp <fernandoandradesouto@hotmail.com>
2022-06-28 10:55:39 -0300

    update

--- pages/taloes/criar.js
+++ pages/taloes/criar.js
@@ -1,6 +1,8 @@
 import { useSession, signIn } from "next-auth/react";
 import React, { useState } from "react";
+import styled from "styled-components";
 import { Botao } from "../../components/Styles/Botao/styles";
+
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
@@ -26,52 +28,76 @@ export default function CriarTalao() {
 	}
 	return (
 		<>
-			<form>
-				<label>
+			<Formulario>
+				<Label>
 					talao:
-					<input onChange={handlerChange} type="text" name="talao" />
-				</label>
-				<label>
+					<Input onChange={handlerChange} type="text" name="talao" />
+				</Label>
+				<Label>
 					data:
-					<input onChange={handlerChange} type="text" name="data" />
-				</label>
-				<label>
+					<Input onChange={handlerChange} type="text" name="data" />
+				</Label>
+				<Label>
 					cliente:
-					<input onChange={handlerChange} type="text" name="cliente" />
-				</label>
-				<label>
+					<Input onChange={handlerChange} type="text" name="cliente" />
+				</Label>
+				<Label>
 					tel:
-					<input onChange={handlerChange} type="text" name="tel" />
-				</label>
-				<label>
+					<Input onChange={handlerChange} type="text" name="tel" />
+				</Label>
+				<Label>
 					produto:
-					<input onChange={handlerChange} type="text" name="produto" />
-				</label>
-				<label>
+					<Input onChange={handlerChange} type="text" name="produto" />
+				</Label>
+				<Label>
 					numeracao:
-					<input onChange={handlerChange} type="text" name="numeracao" />
-				</label>
-				<label>
+					<Input onChange={handlerChange} type="text" name="numeracao" />
+				</Label>
+				<Label>
 					descricao:
-					<input onChange={handlerChange} type="text" name="descricao" />
-				</label>
-				<label>
+					<Input onChange={handlerChange} type="text" name="descricao" />
+				</Label>
+				<Label>
 					obs:
-					<input onChange={handlerChange} type="text" name="obs" />
-				</label>
-				<label>
+					<Input onChange={handlerChange} type="text" name="obs" />
+				</Label>
+				<Label>
 					recebidopor:
-					<input onChange={handlerChange} type="text" name="recebidopor" />
-				</label>
-				<label>
+					<Input onChange={handlerChange} type="text" name="recebidopor" />
+				</Label>
+				<Label>
 					loja:
-					<input onChange={handlerChange} type="text" name="loja" />
-				</label>
+					<Input onChange={handlerChange} type="text" name="loja" />
+				</Label>
 				<Botao type="submit">Criar</Botao>
-			</form>
+			</Formulario>
 		</>
 	);
 }
+const Formulario = styled.form`
+	display: grid;
+	column-gap: 10px;
+	row-gap: 15px;
+	background-color: #efe4a3;
+	grid-template: auto auto auto auto;
+`;
+const Label = styled.label`
+	color: #2d2d2d;
+	font-weight: 700;
+`;
+const Input = styled.input`
+	background: none;
+	border: none;
+	border-bottom: 2px solid #2d2d2d;
+	padding: 10px;
+	margin-left: 5px;
+	font-weight: 700;
+	&:focus{
+		outline: none;
+		border-bottom: 2px solid #D13737;
+	}
+`;
+
 
 export async function getServerSideProps({ query }) {
 	if (query.talao) {
