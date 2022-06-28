0bbda8f9dd76bf495ebf829ebd104a56ed48b5c1
Mahdasah <headbanger.87@hotmail.com>
2022-06-28 01:18:40 -0300

    update

--- components/Styles/Botao/styles.js
+++ components/Styles/Botao/styles.js
@@ -6,12 +6,7 @@ export const Botao = styled.button`
 	color: white;
 	border-radius: 5px;
 	&:hover {
-<<<<<<< HEAD
-		background-color: black;
-		color: violet;
-=======
 		background-color: white;
-		color: #FFFFFF;
->>>>>>> d913476a2d8134a554c3c9dd83d16d2e6bb49fd8
+		color: #ffffff;
 	}
 `;
