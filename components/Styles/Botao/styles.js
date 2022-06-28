01f1dada407ccbdea826578a3a8df9711c56a5e6
soutovp <fernandoandradesouto@hotmail.com>
2022-06-28 01:26:16 -0300

    so

--- components/Styles/Botao/styles.js
+++ components/Styles/Botao/styles.js
@@ -6,12 +6,7 @@ export const Botao = styled.button`
 	color: white;
 	border-radius: 5px;
 	&:hover {
-<<<<<<< HEAD
-		background-color: white;
-		color: #ffffff;
-=======
 		background-color: black;
 		color: #000;
->>>>>>> 4161a876d66bb62803bb93adb4967798c147996e
 	}
 `;
