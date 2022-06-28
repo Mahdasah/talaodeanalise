411b42a50012c34be8a38ccfa8e359917bdf99a4
Mahdasah <headbanger.87@hotmail.com>
2022-06-28 01:58:24 -0300

    update

--- components/Styles/Botao/styles.js
+++ components/Styles/Botao/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Botao = styled.button`
 	padding: 10px;
-	background-color: blue;
+	background-color: black;
 	color: white;
 	border-radius: 101px;
 	&:hover {
