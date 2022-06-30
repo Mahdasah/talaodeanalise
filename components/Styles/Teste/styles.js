1048fa28ab9be41a6cc1919693918271998306ad
Mahdasah <headbanger.87@hotmail.com>
2022-06-30 01:39:06 -0300

    update

--- components/Styles/Teste/styles.js
+++ components/Styles/Teste/styles.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const TesteS = styled.button`
 	padding: 5px;
-	background-color: black;
+	background-color: ${(props) => (props.red ? "red" : "black")};
 	color: white;
 	border-radius: 5px;
 	&:hover {
-		background-color: white;
-		color: black;
+		background-color: ${(props) => (props.red ? "blue" : "white")};
+		color: ${(props) => (props.red ? "white" : "black")};
 	}
 `;
